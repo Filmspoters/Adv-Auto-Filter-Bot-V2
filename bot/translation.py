dd82e2cddcfc0120fc1322d04d8e8012479df99c
Filmspoters <89377498+Filmspoters@users.noreply.github.com>
2021-08-26 12:37:12 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -5,12 +5,13 @@
 class Translation(object):
     
     START_TEXT = """<b>Hey {}!!</b>
-<i>Am Just A Advance Auto Filter Bot....😉
+<i>Am Just A Auto Filter Bot....😉
 
-Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
+Our group https://t.me/Filmspoters
+Our channel https://t.me/filmspoter
 
-For More Details Click Help Button Below..
-@CrazyBotsz
+For More Details contact @Hacker_A10
+
 </i>"""    
     
     HELP_TEXT = """
@@ -21,34 +22,13 @@ For More Details Click Help Button Below..
 -> Add Me To Your Desired Channel
 </i>
 
-<b>Bot Commands (Works Only In Groups) :</b>
 
-    -> <code>/add chat_id</code>
-                OR                  - To Connect A Group With A Channel (Bot Should Be Admin With Full Previlages In Both Group And Channel)
-     <code>/add @Username</code>
-     
-    -> <code>/del chat_id</code>
-                OR                  - To disconnect A Group With A Channel
-     <code>/del @Username</code>
-     
-    -> <code>/delall</code>  - This Command Will Disconnect All Connected Channel With The Group And Deletes All Its File From DB
-    
-    -> <code>/settings</code> -  This Command Will Display You A Settings Pannel Instance Which Can Be Used To Tweek Bot's Settings Accordingly
-
-            -> <code>Channel</code> - Button Will Show You All The Connected Chats With The Group And Will Show Buttons Correspnding To There Order For Furthur Controls
             
-            -> <code>Filter Types</code> - Button Will Show You The 3 Filter Option Available In Bot... Pressing Each Buttons Will Either Enable or Disable Them And This Will Take Into Action As Soon As You Use Them Without The Need Of A Restart
-
-            -> <code>Configure</code> - Button Will Helps You To Change No. of Pages/ Buttons Per Page/ Total Result Without Acutally Editing The Repo... Also It Provide Option To Enable/Disable For Showing Invite Link In Each Results
-            
-            -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
-            
-@CrazyBotsz
-"""
+@Hacker_A10
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ Creator</b> : <b><i><a>@Hacker_A10
 
 <b>➥ Language</b> : <code>Python3</code>
 
